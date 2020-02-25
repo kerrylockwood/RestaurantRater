85de8675d9c5206c59490365f18addb010ee3e25
Kerry Lockwood <klockwood@celadontrucking.com>
2020-02-25 10:44:22 -0500

    Add project files.

---
+++ RestaurantRater/Models/RestaurantDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class RestaurantDbContext : DbContext  //DbContext is from System.Data.Entity;
+    {
+        //Database connection
+        public RestaurantDbContext() : base("DefaultConnection")  // "DefaultConnection" must match Web.config connectionStrings  Name
+        {   
+        }
+
+        // Database set 
+        public DbSet <Restaurant> Restaurants { get; set; }
+    }
+}
